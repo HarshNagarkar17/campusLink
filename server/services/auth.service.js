ac445e5bf26e2648e735aa677ec1caac194f191d
Harsh Nagarkar <harshvn187@gmail.com>
2023-10-07 09:21:28 +0530

    let user login if verified

--- server/services/auth.service.js
+++ server/services/auth.service.js
@@ -9,7 +9,8 @@ const userService = require("./user.service")
  */
 const validateUser = async(email, passsword) => {
     const user = await userService.getUserbyEmail(email);
-
+    if(user.isActive === false)
+        throw new Error("Please verify account before logging In");
     if(!user || !(await user.matchPassword(passsword)))
         throw new Error("Incorrect email or password");
     return user;
